c74032e7ffc39d234acc09ea7fb50a033521717b
Sammy Mahmoudi <sammymahmoudi@gmail.com>
2024-06-13 21:49:06 -0700

    Global Timer

--- PachiArmy/Program.cs
+++ PachiArmy/Program.cs
@@ -12,7 +12,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddSingleton<PeriodicExecutor>();
+
+// Startup
+GlobalTimer.StartTimer();
+
 
 // Blazorise
 builder.Services
---
+++ PachiArmy/Scripts/GlobalTimer.cs
@@ -0,0 +1,17 @@
+﻿using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace PachiArmy.Scripts
+{
+    public static class GlobalTimer
+    {
+        public static Timer Timer;
+        public static void StartTimer()
+        {
+            Timer = new Timer(1000);
+            Timer.AutoReset = true;
+            Timer.Enabled = true;
+            Timer.Start();
+        }
+    }
+}
