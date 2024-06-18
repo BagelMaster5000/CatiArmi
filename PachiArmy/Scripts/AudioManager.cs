41575f649e84a8339d6ffedadd532a5521ba9f11
Sammy Mahmoudi <sammymahmoudi@gmail.com>
2024-06-17 21:04:12 -0700

    Added sound and music playing functionality

---
+++ PachiArmy/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.JSInterop;
+
+namespace PachiArmy.Scripts
+{
+    public static class AudioManager
+    {
+        private static IJSRuntime JS;
+        public static void InitializeJSRuntime(IJSRuntime js)
+        {
+            JS = js;
+        }
+
+        public static void PlayMusic()
+        {
+            Task.Run(async () => await JS.InvokeVoidAsync("playMusic"));
+        }
+
+        public static void PlaySound(string sound)
+        {
+            Task.Run(async () => await JS.InvokeVoidAsync("playSound", sound));
+        }
+    }
+}
